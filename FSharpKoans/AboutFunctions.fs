161aef5f13b30b94d5f8ebf9e05107d9ed62189e
FourtyThree43 <shaqmwa@outlook.com>
2024-04-29 23:59:11 +0300

    Task 3-ish

--- FSharpKoans/AboutFunctions.fs
+++ FSharpKoans/AboutFunctions.fs
@@ -23,27 +23,27 @@ module ``about functions`` =
     let CreatingFunctionsWithLet() =
         let result1 = add 2 2
         let result2 = add 5 2
-        
-        AssertEquality result1 __
-        AssertEquality result2 __
+
+        AssertEquality result1 4
+        AssertEquality result2 7
 
     [<Koan>]
     let NestingFunctions() =
-        let quadruple x =    
+        let quadruple x =
             let double x =
                 x * 2
 
             double(double(x))
 
         let result = quadruple 4
-        AssertEquality result __
+        AssertEquality result 16
 
     [<Koan>]
     let AddingTypeAnnotations() =
 
         (* Sometimes you need to help F#'s type inference system out with an
            explicit type annotation *)
-    
+
         let sayItLikeAnAuctioneer (text:string) =
             text.Replace(" ", "")
 
@@ -65,8 +65,8 @@ module ``about functions`` =
 
         AssertEquality caffeinatedReply __
 
-        (* NOTE: Accessing the suffix variable in the nested caffeinate function 
-                 is known as a closure. 
-                 
-                 See http://en.wikipedia.org/wiki/Closure_(computer_science) 
+        (* NOTE: Accessing the suffix variable in the nested caffeinate function
+                 is known as a closure.
+
+                 See http://en.wikipedia.org/wiki/Closure_(computer_science)
                  for more about about closure. *)
